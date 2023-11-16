44209ccf1b34497e191e6586995a0705fff15535
navigaid <navigaid@gmail.com>
2023-11-16 13:46:44 +0000

    update web3FromAddress/web3FromSource

--- web3FromAddress.js
+++ web3FromAddress.js
@@ -1,3 +1,11 @@
 let ALICE = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY";
 
 let { signer } = await web3FromAddress(ALICE);
+
+let { signature } = await signer.signRaw({
+  address: ALICE,
+  data: "Gear Shell",
+  type: "payload",
+});
+
+console.log(signature);
