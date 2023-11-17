43ab354f925037902de0982d47b21b853cee7c45
navigaid <navigaid@gmail.com>
2023-11-17 03:15:17 +0000

    sed -i -e s,https://no-cors.deno.dev/,,g *.js

--- fetchResources.js
+++ fetchResources.js
@@ -1,5 +1,5 @@
 let metaResp = await fetch(
-  "https://no-cors.deno.dev/https://github.com/btwiuse/gurls/releases/download/v0.0.1/gurls.meta.txt",
+  "https://github.com/btwiuse/gurls/releases/download/v0.0.1/gurls.meta.txt",
 );
 
 let metaText = await metaResp.text();
@@ -7,7 +7,7 @@ let metaText = await metaResp.text();
 console.log(`0x${metaText}`);
 
 let wasmResp = await fetch(
-  "https://no-cors.deno.dev/https://github.com/btwiuse/gurls/releases/download/v0.0.1/gurls.opt.wasm",
+  "https://github.com/btwiuse/gurls/releases/download/v0.0.1/gurls.opt.wasm",
 );
 
 let wasmBuff = await wasmResp.arrayBuffer();
