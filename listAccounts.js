b39ac22472954ed2a6b39cb013e56518f0869ac6
navigaid <navigaid@gmail.com>
2023-11-19 11:07:28 +0000

    add isWeb3Injected to listAccounts.js & listWallets.js

--- listAccounts.js
+++ listAccounts.js
@@ -1,3 +1,5 @@
+console.log("detected wallet:", isWeb3Injected);
+
 let accounts = await web3Accounts();
 
 console.log(JSON.stringify(accounts, null, "  "));
--- listWallets.js
+++ listWallets.js
@@ -1,3 +1,5 @@
+console.log("detected wallet:", isWeb3Injected);
+
 let wallets = await web3Enable("Gear Shell");
 
 console.log(JSON.stringify(wallets, null, "  "));
